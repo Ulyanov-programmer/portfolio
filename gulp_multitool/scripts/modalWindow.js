be3d63485806e5a2608597d0c90302c3b4aa625c
Ulyanov-programmer <ccoldatheinrich@yandex.ru>
2022-09-09 18:19:14 +0400

    Fix errors.

--- gulp_multitool/scripts/modalWindow.js
+++
@@ -1,6 +0,0 @@
-import ModalWindowMenu from "./modules/modalWindow.src.js";
-new ModalWindowMenu({
-  modalLinksSelector: "[data-open-modal-id]",
-  modalClosersSelector: "[data-modal-closer]",
-  disableOnEsc: false
-});
--- gulp_multitool/scripts/photoSwipe.js
+++
@@ -1,7 +0,0 @@
-import PhotoSwipeLightbox from "./photoswipe-lightbox.esm.min.js";
-import PhotoSwipe from "./photoswipe.esm.min.js";
-new PhotoSwipeLightbox({
-  gallery: "#portfolio-about-modal__gallery",
-  children: "a",
-  pswpModule: PhotoSwipe
-}).init();
